9e230093295f878bf61623c2920ca87f796f9627
Thiago Leite <thiago.leite@nad1.com.br>
2018-09-12 00:00:33 -0300

    add Header component and container

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Provider } from "react-redux";
 import ProductListContainer from "./containers/ProductListContainer";
 import CheckoutContainer from "./containers/CheckoutContainer";
+import HeaderContainer from "./containers/HeaderContainer";
 import configureStore from "./configureStore";
 
 const store = configureStore();
@@ -11,12 +12,15 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Router>
-          <Switch>
-            <Route path="/" exact component={ProductListContainer} />
-            <Route path="/checkout" exact component={CheckoutContainer} />
-          </Switch>
-        </Router>
+        <Fragment>
+          <HeaderContainer />
+          <Router>
+            <Switch>
+              <Route path="/" exact component={ProductListContainer} />
+              <Route path="/checkout" exact component={CheckoutContainer} />
+            </Switch>
+          </Router>
+        </Fragment>
       </Provider>
     );
   }
---
+++ client/src/containers/HeaderContainer.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import Header from "../components/Header";
+
+class HeaderContainer extends Component {
+  render() {
+    return <Header />;
+  }
+}
+
+export default HeaderContainer;
